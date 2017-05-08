44d7c02bccc0862b38ca6b547467e506c4a720d0
Lu Mingming <migndul@gmail.com>
2017-05-08 14:25:49 +0800

    Design index.html, about.html and error/404.html

---
+++ src/main/java/com/example/web/RootController.java
@@ -0,0 +1,18 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String getAbout() {
+        return "about";
+    }
+}
