f280d9a11932bc54e351d057dedbbb25a8745526
Lu Mingming <migndul@gmail.com>
2017-05-13 22:04:03 +0800

    Modify home page displaying all questionnaires order by createdAt desc

--- src/main/java/com/example/repository/QuestionnaireRepository.java
+++ src/main/java/com/example/repository/QuestionnaireRepository.java
@@ -3,5 +3,9 @@ package com.example.repository;
 import com.example.domain.Questionnaire;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface QuestionnaireRepository extends CrudRepository<Questionnaire, Long> {
+
+    List<Questionnaire> findByOrderByCreatedAtDesc();
 }
--- src/main/java/com/example/web/RootController.java
+++ src/main/java/com/example/web/RootController.java
@@ -1,14 +1,24 @@
 package com.example.web;
 
+import com.example.domain.Questionnaire;
+import com.example.repository.QuestionnaireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class RootController {
 
+    @Autowired
+    private QuestionnaireRepository questionnaireRepository;
+
     @GetMapping("/")
-    public String getIndex() {
-        return "index";
+    public ModelAndView getIndex() {
+        List<Questionnaire> questionnaires = questionnaireRepository.findByOrderByCreatedAtDesc();
+        return new ModelAndView("index", "questionnaires", questionnaires);
     }
 
     @GetMapping("/about")
