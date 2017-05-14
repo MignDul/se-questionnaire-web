6b08bc3a2e11917762e639a602eb3f9ef79611e1
Lu Mingming <migndul@gmail.com>
2017-05-15 00:46:17 +0800

    Add basic security (only one in-memory user)

---
+++ src/main/java/com/example/web/UserController.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "user/login";
+    }
+}
